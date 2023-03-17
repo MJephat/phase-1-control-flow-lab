46f9fb553c4ff243f00f74d52cc6316e81ecc7fd
MJephat <mukundijephat22@gmail.com>
2023-03-17 16:30:28 +0300

    reviewed my code

--- index.js
+++ index.js
@@ -4,36 +4,44 @@ function scuberGreetingForFeet(a){
   if (a <= 400){
    return result = 'This one is on me!';
   }
-  else if (a <= 2499){
-   return result = 'I will gladly take your thirty bucks.';
+
+  else if(a>2000 && a<2500){
+    return result = 'I will gladly take your thirty bucks.';
   }
-  else  (a > 2500);{
-   return result ='No can do.'
+
+  else if(a>2500){
+    return result = 'No can do.';
   }
-  }
-  scuberGreetingForFeet(400)
-  function ternaryCheckCity(b){
-    // Write your code here!
-    let result;
-    if (b === 'NYC'){
-      return result = 'Ok, sounds good.'
-    }
-    else {
-      return result = 'No go.'
-    }
-  }
-  ternaryCheckCity('Pittsburgh')
-  function switchOnCharmFromTip(c){
-    // Write your code here!
-    let result;
-    if (c === 'generous'){
-      return result = 'Thank you so much.'
-    }
-    else if (c === 'not as generous'){
-      return result = 'Thank you.'
-    }
-    else {
-      return result = 'Bye.'
-    }
-  }
-  switchOnCharmFromTip(generous)
+
+  
+}
+
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(2001));
+console.log(scuberGreetingForFeet(2501));
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good.": "No go.";
+}
+
+console.log(ternaryCheckCity("NYC"));
+
+
+// 
+const tip = 'generous';
+ function switchOnCharmFromTip(tip) {
+   switch (tip) {
+     case 'generous':
+     return ('Thank you so much.');
+     
+     
+     case 'not as generous':
+     return ('Thank you.');
+     
+     
+     default:
+     return ('Bye.');
+   }
+ }
+
+console.log(switchOnCharmFromTip('generous'));
